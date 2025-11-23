f215acfeae3aa58d0fb63fb653a084b3bdfdbe44
hasanaat <arsheema619@gmail.com>
2025-11-23 16:59:27 +0530

    arrays and objects

--- inference/03-arrays-and-objects.ts
+++ inference/03-arrays-and-objects.ts
@@ -1,7 +1,8 @@
     let numbers = [1, 2, 3]; // Inferred as number[]
     let mixed = [1, "hello", true]; // Inferred as (number | string | boolean)[]
     let user = {
-      
+
       name: "Alice",
+      
       age: 25,
     }; // Inferred as { name: string; age: number; }
