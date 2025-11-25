710aac263d1f9493845f16b1d21791d5ae0801a4
hasanaat <arsheema619@gmail.com>
2025-11-25 15:20:32 +0530

    ts

--- inference/02.ts
+++ inference/02.ts
@@ -3,7 +3,6 @@
     const age = 10;
          // Inferred as 30 (literal type for const)
     let score = 100; 
-    
     // Inferred as number
     //Variable Declarations: When a variable is declared and assigned an initial value, 
     //TypeScript infers its type based on that value.
