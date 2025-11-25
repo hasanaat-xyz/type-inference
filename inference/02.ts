aa944ef97bf6622803b11de1eff281c19b4ebf80
hasanaat <arsheema619@gmail.com>
2025-11-25 15:19:41 +0530

    ts

--- inference/02.ts
+++ inference/02.ts
@@ -2,7 +2,6 @@
     // Inferred as string
     const age = 10;
          // Inferred as 30 (literal type for const)
-
     let score = 100; 
     // Inferred as number
     //Variable Declarations: When a variable is declared and assigned an initial value, 
